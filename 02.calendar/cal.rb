f4d0a78572a86d1ce9a23626d58dd89e54866df7
utsubo256 <greeeentea256@gmail.com>
2024-08-19 23:32:19 +0900

    fix: 初日と最終日の変数名から修飾語を削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,23 +10,23 @@ input_month = options["m"]
 target_year = input_year.nil? ? Date.today.year : input_year.to_i
 target_month = input_month.nil? ? Date.today.month : input_month.to_i
 
-first_date_of_month = Date.new(target_year, target_month, 1)
-last_date_of_month = Date.new(target_year, target_month, -1)
+first_date = Date.new(target_year, target_month, 1)
+last_date = Date.new(target_year, target_month, -1)
 
-display_month = first_date_of_month.strftime('%B')
+display_month = first_date.strftime('%B')
 display_days = 'Su Mo Tu We Th Fr Sa'
 
 puts "#{display_month} #{target_year}".center(display_days.size)
 puts display_days
 
-(first_date_of_month..last_date_of_month).each do |date|
+(first_date..last_date).each do |date|
   if date.day == 1
     print date.day.to_s.rjust(2 + date.wday*3, ' ')
   else
     print date.day.to_s.rjust(2)
   end
   date.wday == 6 ? puts : print(' ')
-  puts if date.day == last_date_of_month.day && date.wday != 6
+  puts if date.day == last_date.day && date.wday != 6
 end
 
 puts
