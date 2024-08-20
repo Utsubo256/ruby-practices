04534ee4c9d008160f6199de1096b8843108fbf8
utsubo256 <greeeentea256@gmail.com>
2024-08-20 22:32:06 +0900

    fix: 表示用の月日の変数名を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,11 +10,11 @@ month = options["m"] ? options["m"].to_i : Date.today.month
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
-display_month = first_date.strftime('%B')
-display_days = 'Su Mo Tu We Th Fr Sa'
+target_month = first_date.strftime('%B')
+days = 'Su Mo Tu We Th Fr Sa'
 
-puts "#{display_month} #{year}".center(display_days.size)
-puts display_days
+puts "#{target_month} #{year}".center(days.size)
+puts days
 
 print '   ' * first_date.wday
 (first_date..last_date).each do |date|
