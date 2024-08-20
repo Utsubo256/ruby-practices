8b4bb75fe87f412291ac7dcff6ca6d541e07c1fc
utsubo256 <greeeentea256@gmail.com>
2024-08-20 22:07:13 +0900

    fix: 受け取ったオプション引数の処理方法を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,19 +4,16 @@ require 'date'
 
 options = ARGV.getopts('y:', 'm:')
 
-input_year = options["y"]
-input_month = options["m"]
+year = options["y"] ? options["y"].to_i : Date.today.year
+month = options["m"] ? options["m"].to_i : Date.today.month
 
-target_year = input_year.nil? ? Date.today.year : input_year.to_i
-target_month = input_month.nil? ? Date.today.month : input_month.to_i
-
-first_date = Date.new(target_year, target_month, 1)
-last_date = Date.new(target_year, target_month, -1)
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 
 display_month = first_date.strftime('%B')
 display_days = 'Su Mo Tu We Th Fr Sa'
 
-puts "#{display_month} #{target_year}".center(display_days.size)
+puts "#{display_month} #{year}".center(display_days.size)
 puts display_days
 
 print '   ' * first_date.wday
