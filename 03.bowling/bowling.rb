e8ff8f6131e39be355bdad44048e3b2448c85b10
utsubo256 <greeeentea256@gmail.com>
2024-08-22 22:52:30 +0900

    refactor: rubocopの指摘点を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# !/usr/bin/env ruby
 
 score = ARGV[0].gsub(/X/, '10,0')
 shots = score.split(',').map(&:to_i)
@@ -6,20 +8,21 @@ frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, index|
-  if index == 9
-    point += frames.slice(9..-1).flatten.sum
-    break
-  elsif frame.first == 10
-    if frames[index+1].first == 10
-      point += 20 + frames[index+2].first
+  point +=
+    if index == 9
+      frames.slice(9..-1).flatten.sum
+      break
+    elsif frame.first == 10
+      if frames[index + 1].first == 10
+        20 + frames[index + 2].first
+      else
+        10 + frames[index + 1].sum
+      end
+    elsif frame.sum == 10
+      10 + frames[index + 1].first
     else
-      point += 10 + frames[index+1].sum
+      frame.sum
     end
-  elsif frame.sum == 10
-    point += 10 + frames[index+1].first
-  else
-    point += frame.sum
-  end
 end
 
 puts point
