2a799fc6755a74e8f3b047526c0b392968a79008
utsubo256 <greeeentea256@gmail.com>
2024-08-27 09:58:58 +0900

    fix: ストライクだった場合の10点がマジックナンバーになっていた箇所を定数化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,19 +5,21 @@ score = ARGV[0].gsub(/X/, '10,0')
 shots = score.split(',').map(&:to_i)
 frames = shots.each_slice(2).to_a
 
+STRIKE = 10
+
 point = 0
 frames.each_with_index do |frame, index|
   point +=
     if index == 9
       frames.slice(9..-1).flatten.sum
-    elsif frame.first == 10
-      if frames[index + 1].first == 10
+    elsif frame.first == STRIKE
+      if frames[index + 1].first == STRIKE
         20 + frames[index + 2].first
       else
-        10 + frames[index + 1].sum
+        STRIKE + frames[index + 1].sum
       end
-    elsif frame.sum == 10
-      10 + frames[index + 1].first
+    elsif frame.sum == STRIKE
+      STRIKE + frames[index + 1].first
     else
       frame.sum
     end
